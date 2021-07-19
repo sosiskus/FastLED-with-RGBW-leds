3ebc46bf3c02300b9a56094cc3497ff51f622df7
Nikita <62812039+robotics-kid@users.noreply.github.com>
2021-07-19 22:49:09 +0300

    Removed 'register' error on c++ 17

--- src/fastpin.h
+++ src/fastpin.h
@@ -58,11 +58,11 @@ public:
 	inline void strobe() __attribute__ ((always_inline)) { toggle(); toggle(); }
 	inline void toggle() __attribute__ ((always_inline)) { *mInPort = mPinMask; }
 
-	inline void hi(register port_ptr_t port) __attribute__ ((always_inline)) { *port |= mPinMask; }
-	inline void lo(register port_ptr_t port) __attribute__ ((always_inline)) { *port &= ~mPinMask; }
-	inline void set(register port_t val) __attribute__ ((always_inline)) { *mPort = val; }
+	inline void hi( port_ptr_t port) __attribute__ ((always_inline)) { *port |= mPinMask; }
+	inline void lo( port_ptr_t port) __attribute__ ((always_inline)) { *port &= ~mPinMask; }
+	inline void set( port_t val) __attribute__ ((always_inline)) { *mPort = val; }
 
-	inline void fastset(register port_ptr_t port, register port_t val) __attribute__ ((always_inline)) { *port  = val; }
+	inline void fastset( port_ptr_t port,  port_t val) __attribute__ ((always_inline)) { *port  = val; }
 
 	port_t hival() __attribute__ ((always_inline)) { return *mPort | mPinMask;  }
 	port_t loval() __attribute__ ((always_inline)) { return *mPort & ~mPinMask; }
@@ -186,11 +186,11 @@ public:
 
 	inline static void toggle() __attribute__ ((always_inline)) { *sInPort = sPinMask; }
 
-	inline static void hi(register port_ptr_t port) __attribute__ ((always_inline)) { *port |= sPinMask; }
-	inline static void lo(register port_ptr_t port) __attribute__ ((always_inline)) { *port &= ~sPinMask; }
-	inline static void set(register port_t val) __attribute__ ((always_inline)) { *sPort = val; }
+	inline static void hi( port_ptr_t port) __attribute__ ((always_inline)) { *port |= sPinMask; }
+	inline static void lo( port_ptr_t port) __attribute__ ((always_inline)) { *port &= ~sPinMask; }
+	inline static void set( port_t val) __attribute__ ((always_inline)) { *sPort = val; }
 
-	inline static void fastset(register port_ptr_t port, register port_t val) __attribute__ ((always_inline)) { *port  = val; }
+	inline static void fastset( port_ptr_t port,  port_t val) __attribute__ ((always_inline)) { *port  = val; }
 
 	static port_t hival() __attribute__ ((always_inline)) { return *sPort | sPinMask;  }
 	static port_t loval() __attribute__ ((always_inline)) { return *sPort & ~sPinMask; }
@@ -225,11 +225,11 @@ public:
 
 	inline static void toggle() __attribute__ ((always_inline)) { }
 
-	inline static void hi(register port_ptr_t port) __attribute__ ((always_inline)) { }
-	inline static void lo(register port_ptr_t port) __attribute__ ((always_inline)) { }
-	inline static void set(register port_t val) __attribute__ ((always_inline)) { }
+	inline static void hi( port_ptr_t port) __attribute__ ((always_inline)) { }
+	inline static void lo( port_ptr_t port) __attribute__ ((always_inline)) { }
+	inline static void set( port_t val) __attribute__ ((always_inline)) { }
 
-	inline static void fastset(register port_ptr_t port, register port_t val) __attribute__ ((always_inline)) { }
+	inline static void fastset( port_ptr_t port,  port_t val) __attribute__ ((always_inline)) { }
 
 	static port_t hival() __attribute__ ((always_inline)) { return 0; }
 	static port_t loval() __attribute__ ((always_inline)) { return 0;}
